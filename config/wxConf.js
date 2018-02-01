26a804c9717e224079049efd92d75183985b6ce7
daiyang <505955007@qq.com>
2018-02-01 13:26:08 +0800

    init

--- config/wxConf.js
+++ config/wxConf.js
@@ -10,8 +10,8 @@ wxConf.getTokenGrantType="client_credential";
 //  获取openid grant_type
 wxConf.userInfoGrantType="authorization_code";
 //  appId
-wxConf.appId="wx6844940cd71c6d78";
+wxConf.appId="";
 //  secret
-wxConf.secret="44a3fd8de54f3f4655309c98a353d45f";
+wxConf.secret="";
 
 module.exports = wxConf;
